886ac84e476eb8d2c385c6e6aae85eefe061680d
Илья Абрамов <ilya.abramov.2002@inbox.ru>
2025-02-07 15:33:18 +0300

    added checks 1-4

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 def check_1_existence(path, type):
     result = False
@@ -10,17 +11,33 @@ def check_1_existence(path, type):
         raise ValueError
     return result
 
-def check_2_permissions(path, permissions):
+def check_2_permissions(path, correct_permissions):
     real_permissions = oct(os.stat(path).st_mode)[-3:]
     result = False
     if real_permissions == permissions:
         result = True
     return result
 
-def check_3_owner(path, owner):
-    real_permissions = oct(os.stat(path).st_mode)[-3:]
+def check_3_owner(path, correct_owner):
+    try:
+        real_owner = Path(path).owner() # TODO: Check if implemented on Astra
+    except NotImplementedError:
+        real_owner = ""
+
     result = False
-    if real_permissions == permissions:
+    if real_owner == correct_owner:
+        result = True
+
+    return result
+
+def check_4_group(path, correct_group):
+    try:
+        real_owner = Path(path).owner() # TODO: Check if implemented on Astra
+    except NotImplementedError:
+        real_owner = ""
+
+    result = False
+    if real_owner == correct_owner:
         result = True
 
     return result
