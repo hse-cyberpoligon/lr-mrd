a43e8cfa50a96f87dff2d1dc869509adcfa55a07
Hierugu <ilya.abramov.2002@inbox.ru>
2025-02-09 00:17:51 +0300

    V2.0

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
-from variants import variant_1
+from variants import variants
+from transliterate import translit # type: ignore
 from utils    import *
-import requests, time, os
+import requests, time, os # type: ignore
+import json
 
+GRP = False # GENERATE_RANDOM_PERMISSIONS
+SWW = True  # SHOW_WHATS_WRONG
 
-def request_task(fio):
-    url = "http://172.18.4.200:8080/start"
+def request_task(surname, name):
+    print("Запрос заданий для выполнения...")
+    url = "http://172.18.4.200:8080/api/start"
     payload = {
-        "username": fio,
-        "lab": "Мандатное разграничение доступа Astra Linux",
+        "username":   surname + "_" + name,
+        "pnet_login": "",
+        "lab":        "Мандатное разграничение доступа Astra Linux",
+        "lab_slug":   "mandatnoe-razgranichenie-dostupa-astra-linux"
     }
-    response = requests.get(url, params=payload)
+    payload = json.dumps(payload).encode('utf-8')
+
+    response = requests.request("GET", url, data=payload)
+    response.encoding = 'utf-8'
+    response = json.loads(response.text)
+    return response
+
+def finish_task(surname, name):
+    url = "http://172.18.4.200:8080/api/end"
+    payload = {
+        "username":   surname + "_" + name,
+        "pnet_login": "",
+        "lab":        "Мандатное разграничение доступа Astra Linux",
+        "lab_slug":   "mandatnoe-razgranichenie-dostupa-astra-linux"
+    }
+    payload = json.dumps(payload).encode('utf-8')
+    response = requests.request("POST", url, data=payload)
+    response.encoding = 'utf-8'
+    response = json.loads(response.text)
     return response
 
 def generate_personal_task(files, surname):
     for f in files:
-        for k in f.keys():
-            f[k]  = f[k].replace('SURNAME', surname)
+        for k in ['path', 'owner', 'group']:
+            f[k]  = f[k].replace('SURNAME', translit(surname.lower(),'ru', reversed=True))
+        if GRP:
+            f['permissions'] = random_permissions() 
     return files
 
 def main():
-    surname     = input("Введите вашу Фамилию: ")
-    name        = input("Введите ваше Имя: ")
-    response    = {'variant': 1, 'tasks' : [1, 2, 3]} # response = request_task(fio)
-    files       = generate_personal_task(variant_1, surname)
+    os.system('clear')
+    surname     = input("Введите Фамилию: ")
+    name        = input("Введите Имя:     ")
+    response    = request_task(surname, name) # response    = {'variant': 1, 'tasks' : [1, 2, 3, 4, 5]}
+    if 'variant' not in response.keys() or 'tasks' not in response.keys():
+        print("Ошибка получения заданий для выполнения!")
+        exit()
+    files       = generate_personal_task(variants[response['variant']-1], surname)
     max_result  = len(response['tasks']) * len(files)
     time_passed = 0
     result      = 0
@@ -31,47 +62,87 @@ def main():
     while True:
         os.system('clear')
         print(f"{surname} {name}, Вариант {response['variant']}")
-        print(f"Тебе надо выполнить задания {response['tasks']}")
+        print(f"Тебе надо выполнить задания:\n{response['tasks']}")
         print(f"Прошло времени: {time_passed//60:02}:{time_passed%60:02}")
-        print(f"Текущий балл: {result} из {max_result}")
+        print(f"Текущий балл:   {result:02}/{max_result:02}")
         print(f"======================================")
-
+            
         result = 0
         for f in files:
-            if 1 in response['tasks']:
+            if "1" in response['tasks']:
                 if check_1_exist(f['path'], f['type']):
                     result += 1
                 else:
-                    print(f"Объект: {f['path']}\nОшибка: Объект не существует")
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Объект не существует")
                     break
-            if 2 in response['tasks']:
+            if "2" in response['tasks']:
                 if check_2_perms(f['path'], f['permissions']):
                     result += 1
                 else:
-                    print(f"Объект: {f['path']}\nОшибка: Неверные права доступа")
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверные права доступа")
+                        print(f"Должно быть: {f['permissions']}")
                     break
-            if 3 in response['tasks']:
+            if "3" in response['tasks']:
                 if check_3_owner(f['path'], f['owner']):
                     result += 1
                 else:
-                    print(f"Объект: {f['path']}\nОшибка: Неверный владелец объекта")
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверный владелец объекта")
+                        print(f"Должно быть: {f['owner']}")
                     break
-            if 4 in response['tasks']:
+            if "4" in response['tasks']:
                 if check_4_group(f['path'], f['group']):
                     result += 1
                 else:
-                    print(f"Объект: {f['path']}\nОшибка: Неверная группа объекта")
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверная группа объекта")
+                        print(f"Должно быть: {f['group']}")
+                    break
+            if "5" in response['tasks']:
+                if check_5_privacy(f['path'], f['privacy_label']):
+                    result += 1
+                else:
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверный уровень конфиденциальности")
+                        print(f"Должно быть: {f['privacy_label']}")
+                    break
+            if "6" in response['tasks']:
+                if check_6_integrity(f['path'], f['integrity']):
+                    result += 1
+                else:
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверный уровень целостности")
+                        print(f"Должно быть: {f['integrity']}")
+                    break
+            if "7" in response['tasks']:
+                if check_7_categories(f['path'], f['categories']):
+                    result += 1
+                else:
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверно присвоены категории")
+                        print(f"Должно быть: {f['categories']}")
+                    break
+            if "8" in response['tasks']:
+                if check_8_flags(f['path'], f['flags']):
+                    result += 1
+                else:
+                    if SWW:
+                        print(f"Объект: {f['path']}\nОшибка: Неверно присвоены специальные флаги")
+                        print(f"Должно быть: {f['flags']}")
                     break
         if max_result == result:
             break
         time.sleep(1)
         time_passed += 1
+        
 
     print(f"Лабораторная работа завершена!")
     print(f"Итоговый балл: {result} из {max_result}")
+    response = finish_task(surname, name)
+    print(f"Ответ сервера: {response['message']}")
     exit()
-
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
-import os
+import os, random
+import subprocess
 from pathlib import Path
+
+
+def random_permissions():
+    r07 = lambda: str(random.randint(0, 7)) 
+    return r07() + r07() + r07()
+
+
+def get_mrd_properties(path):
+    # 0 - метка безопасности 1 - уровень целостности 2 - [категории]  3 - [флаги]
+    if not os.path.exists(path):
+        raise FileNotFoundError
+    properties = subprocess.check_output(['pdp-ls', '-Md', path], text=True)
+    properties = properties.split()[-2].split(':')
+    properties[2] = [] if properties[2] == 'Нет' else properties[2].split(',')
+    properties[3] = [] if properties[3] == '0x0' else properties[3].split(',')
+    return properties
 
 
 def check_1_exist(path, type):
@@ -16,24 +33,19 @@ def check_1_exist(path, type):
 def check_2_perms(path, correct_permissions):
     if not os.path.exists(path):
         return False
+    
     real_permissions = oct(os.stat(path).st_mode)[-3:]
-    result = False
-    if real_permissions == correct_permissions:
-        result = True
+    result = True if real_permissions == correct_permissions else False
+
     return result
 
 
 def check_3_owner(path, correct_owner):
     if not os.path.exists(path):
         return False
-    try:
-        real_owner = Path(path).owner() # TODO: Check if implemented on Astra
-    except NotImplementedError:
-        real_owner = ""
-    print(real_owner)
-    result = False
-    if real_owner == correct_owner:
-        result = True
+    
+    real_owner = Path(path).owner()
+    result = True if real_owner == correct_owner else False
 
     return result
 
@@ -41,13 +53,47 @@ def check_3_owner(path, correct_owner):
 def check_4_group(path, correct_group):
     if not os.path.exists(path):
         return False
-    try:
-        real_owner = Path(path).owner() # TODO: Check if implemented on Astra
-    except NotImplementedError:
-        real_owner = ""
+    
+    real_owner = Path(path).owner()
+    result = True if real_owner == correct_group else False
 
-    result = False
-    if real_owner == correct_owner:
-        result = True
+    return result
+
+
+def check_5_privacy(path, correct_privacy):
+    if not os.path.exists(path):
+        return False
+    
+    real_privacy = get_mrd_properties(path)[0]
+    result = True if real_privacy == correct_privacy else False
+    
+    return result
+
+def check_6_integrity(path, correct_integrity):
+    if not os.path.exists(path):
+        return False
+    
+    real_integrity = get_mrd_properties(path)[1]
+    result = True if real_integrity == correct_integrity else False
+
+    return result
+
+def check_7_categories(path, correct_categories):
+    if not os.path.exists(path):
+        return False
+    
+    result = True
+    real_categories = get_mrd_properties(path)[2]
+    result = True if set(real_categories) == set(correct_categories) else False
+
+    return result
+
+def check_8_flags(path, correct_flags):
+    if not os.path.exists(path):
+        return False
+    
+    result = True
+    real_flags = get_mrd_properties(path)[3]
+    result = True if set(real_flags) == set(correct_flags) else False
 
     return result
